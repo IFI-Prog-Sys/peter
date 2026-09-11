f1ec212383b689fbdeabc23ceb08d12d84f29a2e
rex0515 <batuolcay05@gmail.com>
2026-09-11 18:51:05 +0200

    Initial Commit

---
+++ discord-bot/src/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from utils import *
+from peter import Peter
+
+def main():
+	log_path = r"./logs/latest.log"
+	logger = get_logger(__name__, log_path)
+
+	config = Config(
+		logger,
+		# general config
+		"intern_role_id", "target_guild_id",
+		# socket config
+		socket_bind_addr="localhost", socket_bind_port=42069, socket_max_connections=5,
+	)
+
+	with open("config.yaml", 'r') as stream:
+		config.load(stream)
+
+	pete = Peter(logger, config)
+	discord_token = get_env('DISCORD_BOT_TOKEN')
+	pete.run(discord_token)
+
+if __name__ == '__main__':
+	main()
